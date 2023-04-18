78ca53e74ee5bd78ca35e41d7f3fa7866ec95bd3
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-18 16:07:02 +0530

    read two numbers P and Q subtract into third

---
+++ untitled4.py
@@ -0,0 +1,6 @@
+#WAP to read two numbers P and Q subtract  and print subtraction into third  variable total
+p=int(input("enter a number"))
+q=int(input("enter a second number"))
+r=int(input("enter a second number"))
+a=p-q-r
+print(a)
