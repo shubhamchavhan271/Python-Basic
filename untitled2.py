a65b532b14c5c4796e0d757a81ee7c34285e2be1
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-18 15:58:01 +0530

    find its area and perimeter•

---
+++ untitled2.py
@@ -0,0 +1,7 @@
+#WAP to read length & breadth of Rectangle and find its area and perimeter• A=L*B • P=2*(L+B)
+l=int(input("enter a number"))
+b=int(input("enter second a number"))
+A=l*b
+P=2*(l+b)
+print(A)
+print(P)
