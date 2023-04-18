58fc00908dbe6d47fe8e61ed93ff8aca304d136f
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-18 16:11:47 +0530

    read marks for 5 subjects and find total marks

---
+++ untitled8.py
@@ -0,0 +1,8 @@
+#Program to read marks for 5 subjects and find total marks.
+a=int(input("enter a maths marks"))
+b=int(input("enter a science marks"))
+c=int(input("enter a chem marks"))
+d=int(input("enter a marathi marks"))
+e=int(input("enter a english marks"))
+t=a+b+c+d+e
+print(t)
