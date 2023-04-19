a2ca442cf689df8116b5c3ceb06b9900389fc8d0
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 11:27:15 +0530

    positive or negative or zero using elif statement

---
+++ ladderif8.py
@@ -0,0 +1,8 @@
+#The given number is positive or negative or zero using elif statement.
+n= int(input("enter a number"))
+if n>0:
+    print("it is positive number")
+elif n<0:
+    print("it is an negative number")
+else:
+    print("zero")
