ceaef369273f5c5647fef54c4189eb63f4483d97
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:25:30 +0530

    a person is senior citizen or not

---
+++ untitled25.py
@@ -0,0 +1,8 @@
+#Write a program to check whether a person is senior citizen or not. (senior>=60)
+a=int(input("enter age of person"))
+if a>=60:
+    print("senior citizen")
+else:
+    print("not a senior citizen")
+
+
