89f311d74740ba269f76bb2a5bd4a7f36882582e
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:24:55 +0530

    person is eligible for voting or not

---
+++ untitled24.py
@@ -0,0 +1,7 @@
+#Write a program to check whether a person is eligible for voting or not.(voting age >=18)
+a=int(input("enter age of person"))
+if a>=18:
+    print("voting age")
+else:
+    print("not avoting age")
+
