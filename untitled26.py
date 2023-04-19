1400de47bf5ecb190521a2e89470587f49979b51
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:26:13 +0530

    find the lowest number out of two numbers

---
+++ untitled26.py
@@ -0,0 +1,8 @@
+#Write a program to find the lowest number out of two numbers excepted from user
+
+a=int(input("enter a first no"))
+b=int(input("enter a 2nd no"))
+if a<b:
+    print(a)
+else:
+    print(b)
