af88e7bc7fa3cd7c2bcaaa049d616d66e4c2e98f
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 12:21:15 +0530

    rint all natural numbers from 1 to n

---
+++ whileloop1.py
@@ -0,0 +1,12 @@
+''' program to print all natural numbers from 1 to n
+Input
+Input upper limit: 7
+Output
+Natural numbers from 1 to 10: 1, 2, 3, 4, 5, 6, 7'''
+
+
+n=int(input("enter a number "))
+a=1
+while a<=7:
+    print(a)
+    a+=1
