80c96f32d498b7518456586897995d47c2677b00
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:22:17 +0530

    print the net bonus amount Bonus =  0.05 * Salary

---
+++ untitled20.py
@@ -0,0 +1,11 @@
+#A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years 
+#Ask user for their salary and year of service and print the net bonus amount Bonus =  0.05 * Salary
+s=int(input("enter a salary"))
+y=int(input("enter a year"))
+if y>5:
+    Bonus=0.05*s 
+    print(Bonus)
+
+
+
+
