f925f85a74e51208233945a7ad2c03abfb6ddf94
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:22:57 +0530

    check if it is square or not.

---
+++ untitled21.py
@@ -0,0 +1,8 @@
+# Take values of length and breadth of a rectangle from user and check if it is square or not.
+a=int(input("enter length"))
+b=int(input("enter breadth"))
+if a==b:
+  print ("it is square")
+else:
+    print("it is not square")
+
