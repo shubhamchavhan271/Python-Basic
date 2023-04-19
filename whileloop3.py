8f2c090aa63c7b8476ff40320a8ee10b2ca3523e
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 12:22:52 +0530

    natural numbers in reverse from n to 1

---
+++ whileloop3.py
@@ -0,0 +1,13 @@
+'''   program to print natural numbers in reverse from n to 1
+Input
+Input N: 10
+Output
+Natural numbers from 10-1 in reverse: 
+10, 9, 8, 7, 6, 5, 4, 3, 2, 1'''
+n=int(input("enter a  number"))
+a=1
+while (a<=n):
+    print(n)
+    n-=1
+    
+
