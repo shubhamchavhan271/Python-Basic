65baef0482301ecb10b513e9d49a06ece1923c7d
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 11:51:47 +0530

    count number of occurrences of x in the given list

---
+++ list10.py
@@ -0,0 +1,11 @@
+#Given a list in Python and a number x, count number of occurrences of x in the given list.
+list=[1,2,3,4,5,6,7,8]
+count=0
+x=8
+for ele in list:    
+    if ele==x:
+        count=count+1
+print(x,"has occurred",count," times") #why this use command
+
+
+
