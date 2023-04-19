d0a306bed8bc844eae38682c98143b5a0cef378e
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:24:27 +0530

    print absolute value of a number

---
+++ untitled23.py
@@ -0,0 +1,9 @@
+#Write a program to print absolute value of a number entered by user. E.g.-
+#INPUT: 1        OUTPUT: 1
+#INPUT: -1        OUTPUT: 1
+
+n=int(input("enter a number"))
+if n==1 or n==-1:
+    print(1)
+else:
+    print()
