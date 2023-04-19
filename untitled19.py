878355c90445724d47927e891d11f771bf76baab
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:21:08 +0530

    number  is divisible by 2 and 3 both.

---
+++ untitled19.py
@@ -0,0 +1,7 @@
+#Write a program to whether a number (accepted from user) is divisible by 2 and 3 both.
+n=int(input("enter a no"))
+if n%2==0 and n%3==0:
+    print("Divisible by 2 and 3")
+else:
+    print("not Divisible")
+
