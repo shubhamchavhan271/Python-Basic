ce348f95a092838e03efbbcbd1ba73f5ac1099b1
shubham chavhan <96036080+shubhamchavhan271@users.noreply.github.com>
2023-04-19 10:26:59 +0530

    whether a number from useris positive or negative.

---
+++ untitled27.py
@@ -0,0 +1,6 @@
+#Write a program to check whether a number (accepted from user) is positive or negative.
+a=int(input("enter a no"))
+if a>0:
+    print("it is an positive no")
+else:
+    print("it is an negative no")
